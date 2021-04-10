af001f3a3bbbc6a5beb940dd7eb644781cd8bb5d
EnzoLivelli2000 <enzo.livelli@gmail.com>
2021-04-10 03:18:35 -0500

    se trabajo lo siguiente: el algoritmo ya resuelve le problema la rubrica se agrego el algoritmo de deteccion de sexo por peso, talla, altura

--- Juego/juego.py
+++ Juego/juego.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pydotplus
 from IPython.display import Image
 from sklearn.tree import export_graphviz
+from sklearn.preprocessing import LabelEncoder
 import graphviz
 
 decisionTree = tree.DecisionTreeClassifier()
@@ -14,33 +15,54 @@ juegoTennis['humedo'] = ['alto', 'alto', 'alto', 'alto', 'normal', 'normal', 'no
 juegoTennis['velocidadViento'] = ['bajo', 'alto', 'bajo', 'bajo', 'bajo', 'alto', 'alto', 'bajo', 'bajo', 'alto', 'alto']
 juegoTennis['juego'] = ['no','no','si','si','si','no','si','no','si','si','no'] 
 
+inputs = juegoTennis.drop('juego', axis='columns')
+target = juegoTennis['juego']
+
+le_cielo = LabelEncoder()
+le_ambiente = LabelEncoder()
+le_humedo = LabelEncoder()
+le_velocidadViento = LabelEncoder()
+
+inputs['cielo_n'] = le_cielo.fit_transform(inputs['cielo'])
+inputs['ambiente_n'] = le_cielo.fit_transform(inputs['ambiente'])
+inputs['humedo_n'] = le_cielo.fit_transform(inputs['humedo'])
+inputs['velocidadViento_n'] = le_cielo.fit_transform(inputs['velocidadViento'])
+
+print(inputs)
+
+inputs_n = inputs.drop(['cielo', 'ambiente', 'humedo', 'velocidadViento'], axis= 'columns')
+
+print(inputs_n)
+
+decisionTree = decisionTree.fit(inputs_n,juegoTennis['juego'])
+print(decisionTree.score(inputs_n,juegoTennis['juego']))
+
+
+decision = decisionTree.predict([[1,0,1,1]])
+
+if decision == 'si':
+    print(' hoy SI se juega')
+else:
+    print(' hoy NO se juega')
+
+'''
+inputs['le_cielo'] = le_cielo.fit_transform(inputs['company'])
+
 print(juegoTennis)
 one_hot_data = pd.get_dummies(juegoTennis[['cielo', 'ambiente', 'humedo', 'velocidadViento']])
 print(one_hot_data)
 
 decisionTree = decisionTree.fit(one_hot_data,juegoTennis['juego'])
 
+print(decisionTree.score(one_hot_data,juegoTennis['juego']))
+
 dot_data = tree.export_graphviz(decisionTree, out_file='juego.dot', feature_names=list(one_hot_data.columns.values),
 rounded=True, filled=True)
 print(dot_data)
 
+prediction = decisionTree.predict([[0,0,1,0,1,0,0,1,1,0]])
+print(prediction) '''
 
-with open('juego.dot') as f:
+''' with open('juego.dot') as f:
     dot_graph=f.read()
-graphviz.Source(dot_graph).view()
-
-
-""" graph = pydotplus.graph_from_dot_data(dot_data)
-Image(graph.create_png()) """
-
-""" X = [
-    [181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
-    [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 55, 37], [171, 75, 42],
-    [181, 85, 43]
-]
-
-Y = [
-    'hombre','hombre','mujer','mujer',
-    'hombre','hombre','mujer','mujer',
-    'mujer','hombre','hombre'
-] """
+graphviz.Source(dot_graph).view() '''
